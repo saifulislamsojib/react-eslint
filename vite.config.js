af4b4207fd1a1e85f43c59bb20ab290f5c9b8a63
saifulislamsojib <saifulislam.sojib56@gmail.com>
2023-04-27 15:36:56 +0600

    config updated

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    port: 8080,
+    open: true,
+  },
 });
